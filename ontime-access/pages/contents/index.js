7ad6bbfa69a29dfcf73bb5955153692dd5c0081e
SatoCsuke <kubotak143@gmail.com>
2026-02-23 08:23:57 +0900

    Download Pageの非活性ボタンを追加することでダウンロード中であることをわかるようにしました

--- ontime-access/pages/contents/index.js
+++ ontime-access/pages/contents/index.js
@@ -77,11 +77,18 @@ function Contents() {
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h1 className="block text-gray-700 text-2xl font-bold mb-2">Contents</h1>
         <p className="block text-gray-700 text-base mb-4">Welcome, {user.email}!</p>
-        {downloadURL && (
+        {downloadURL ? (
           <a href={downloadURL} download="book.pdf" target="_blank" rel="noopener noreferrer" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
             Download PDF
           </a>
-
+        ) : (
+          <button disabled className="bg-green-500 opacity-50 cursor-not-allowed text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center">
+            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            Preparing Download...
+          </button>
         )}
         {downloadError && <p className="text-red-500">{downloadError}</p>}
         <button
